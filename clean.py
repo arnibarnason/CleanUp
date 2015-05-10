4b5553b8dacbcc95ac56315831aaac40d99df12f
Magnús Sigurðarson <maggisig92@gmail.com>
2015-05-10 22:11:53 +0000

    folder created for seasons

--- clean.py
+++ clean.py
@@ -8,17 +8,19 @@ parser.add_argument('show', help='Name of the show to sort')
 parser.add_argument('folder', help='Path to and the of the folder to contain the show')
 args = parser.parse_args()
 
-s = args.show
+s = args.show.replace(' ', '.')
 x = s.replace(' ', '.')
 for root, dirs, files in os.walk('dl'):
-	for f in files:
+	for g in files:
+		f = re.sub(r'[_\s\.-]+', '.', g)
+		os.rename(os.path.join(os.path.abspath(root), g), os.path.join(os.path.abspath(root), f))
 		found = re.search(x + '.(S([0-9][0-9]))*', f, re.I)
 		if found != None:
 			season = ''
 			if found.group(2): 
-				season = "Season " + found.group(2)
-				print(f, "season: ", season)
-			if not os.path.exists(os.path.join(args.folder, args.show, season)):
-				os.makedirs(os.path.join(args.folder, args.show, season))
-			print(os.path.join(os.path.abspath(root),f))
-			os.rename(os.path.join(os.path.abspath(root),f), os.path.join(args.folder, args.show, season, f))
+				season = "Season." + found.group(2)
+				#print(f, "season: ", season)
+			if not os.path.exists(os.path.join(args.folder, s, season)):
+				os.makedirs(os.path.join(args.folder, s, season))
+			#print(os.path.join(os.path.abspath(root),f))
+			os.rename(os.path.join(os.path.abspath(root),f), os.path.join(args.folder, s, season, f))
